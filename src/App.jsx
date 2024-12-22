0c9bb2a72f1fb83cce46ae81211425c987b1f388
Olha Petrova <olga.petrova.se@gmail.com>
2024-12-22 12:29:39 -0800

    fixed infinite loop with useCallback hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -55,17 +55,17 @@ function App() {
     }    
   }
 
-  function handleRemovePlace() { //func obj is recreated when func App component executes
+  const handleRemovePlace = useCallback(function handleRemovePlace() { //func obj is recreated when func App component executes
     console.log('handleRemovePlace');
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    //setModalIsOpen(false);
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces', JSON.stringify(
       storedIds.filter(id => id !== selectedPlace.current)));
-  }
+  }, []);
 
   return (
     <>
