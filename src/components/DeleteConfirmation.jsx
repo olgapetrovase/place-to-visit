7419a71e749e2d5e2ad526002536a59ab21126ac
Olha Petrova <olga.petrova.se@gmail.com>
2024-12-22 11:22:41 -0800

    UseEffect's cleanup func

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log('timer set');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => { //cleanup func executes before useEffects runs again or before component did unmount
+      console.log('cleaning up timer');
+      clearTimeout(timer);
+    };
+  }, []);  
+  
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
